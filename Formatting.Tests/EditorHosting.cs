98f268a4b9f8d3ee4d36a3d31b335a2162f1ab0c
Luca Duran <luca.duran@hotmail.com>
2015-07-06 16:59:42 -0700

    Add starting point for editor hosting for tests

---
+++ Formatting.Tests/EditorHosting.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.Composition;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Formatting.Tests
+{
+    class EditorHost
+    {
+        private const string EditorAssemblyNames = @"
+            Microsoft.VisualStudio.Platform.VSEditor, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Text.Logic, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Text.UI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Text.UI.Wpf, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Language.StandardClassification, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.CoreUtility, Version=14.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+        ";
+
+        internal static async Task<ExportProvider> CreateExportProviderAsync(ITestOutputHelper logger)
+        {
+            var assemblies = new List<Assembly>();
+            foreach (string line in EditorAssemblyNames.Split('\n'))
+            {
+                string assemblyName = line.Trim();
+                if (!string.IsNullOrEmpty(assemblyName))
+                {
+                    var assembly = Assembly.Load(assemblyName);
+                    assemblies.Add(assembly);
+                }
+            }
+
+            var discovery = new AttributedPartDiscoveryV1(Resolver.DefaultInstance);
+            var parts = await discovery.CreatePartsAsync(assemblies);
+            var catalog = ComposableCatalog.Create(Resolver.DefaultInstance)
+                .AddParts(parts)
+                .WithDesktopSupport()
+                .WithCompositionService();
+            var configuration = CompositionConfiguration.Create(catalog);
+            if (!configuration.CompositionErrors.IsEmpty)
+            {
+                foreach (var error in configuration.CompositionErrors.First())
+                {
+                    logger.WriteLine(error.Message);
+                }
+
+                configuration.ThrowOnErrors();
+            }
+
+            var composition = RuntimeComposition.CreateRuntimeComposition(configuration);
+            var expf = composition.CreateExportProviderFactory();
+            return expf.CreateExportProvider();
+        }
+    }
+}
---
+++ Formatting.Tests/WrappingTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.VisualStudio.Utilities;
+using Xunit.Abstractions;
+
+namespace Formatting.Tests
+{
+    public class WrappingTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public WrappingTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task EmptyFunction()
+        {
+            string original = "foo = function() end";
+            string expected = @"foo = function()
+end";
+            string actual = await FormatAsync(original);
+            Assert.Equal(expected, actual);
+        }
+
+        private async Task<string> FormatAsync(string original)
+        {
+            var ep = await EditorHost.CreateExportProviderAsync(this.testOutputHelper);
+            var editorFacotryService = ep.GetExportedValue<ITextEditorFactoryService>();
+            var textView = editorFacotryService.CreateTextView();
+            var textBuffer = textView.TextBuffer;
+            var textEdit = textBuffer.CreateEdit();
+            textEdit.Replace(0, 0, original);
+            var applied = textEdit.Apply();
+
+            //List<TextEditInfo> textEdits = Formatter.Format(original);
+
+            return applied.GetText();
+        }
+    }
+}
