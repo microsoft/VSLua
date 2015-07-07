41ac601137f5b3e831632b5ec53d45ca682bb6ce
Luca Duran <luca.duran@hotmail.com>
2015-07-07 15:16:57 -0700

    Adds for loop tests

---
+++ Formatting.Tests/ForLoopTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Formatting.Tests
+{
+    internal static class ForLoopTests
+    {
+        internal static void GeneralFunctionTest(string original, string expected)
+        {
+            string actual = Tester.Format(original);
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        internal static void Basic()
+        {
+            string original = "for i = 1, 2 do end";
+            string expected = "for i = 1,2 do end";
+            Assert.Equal(original, expected);
+        }
+
+        [Fact]
+        internal static void NoChange()
+        {
+            string original = "for i = 1,2 do end";
+            string expected = "for i = 1,2 do end";
+            Assert.Equal(original, expected);
+        }
+
+        [Fact]
+        internal static void ManySpaces()
+        {
+            string original = "for i = 1,                    2 do end";
+            string expected = "for i = 1,2 do end";
+            Assert.Equal(original, expected);
+        }
+    }
+}
