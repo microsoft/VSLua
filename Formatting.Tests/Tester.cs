55832f7e5c7848499f51e5312f00fb87dd084cdf
Luca Duran <luca.duran@hotmail.com>
2015-07-08 09:50:57 -0700

    Add Tester interaction with Formatter

--- Formatting.Tests/Tester.cs
+++ Formatting.Tests/Tester.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LanguageModel.Formatting;
 
 namespace Formatting.Tests
 {
@@ -11,11 +12,21 @@ namespace Formatting.Tests
         internal static string Format(string original)
         {
 
+
+
+            List<TextEditInfo> textEdits = Formatter.Format(original);
+
             var factory = new EditorUtils.EditorHostFactory();
             var host = factory.CreateEditorHost();
 
             var buffer = host.CreateTextBuffer(original);
             var edit = buffer.CreateEdit();
+
+            foreach (var textEdit in textEdits)
+            {
+                edit.Replace(textEdit.Start, textEdit.Length, textEdit.ReplacingString);
+            }
+
             var applied = edit.Apply();
 
             return applied.GetText();
