a089c23d74c0eb46f55baf5d8f3f7ae1440d48b0
Luca Duran <luca.duran@hotmail.com>
2015-07-08 11:11:46 -0700

    Add basic functionality to FormattingContext

--- LanguageModel/Formatting/FormattingContext.cs
+++ LanguageModel/Formatting/FormattingContext.cs
@@ -8,7 +8,36 @@ namespace LanguageModel.Formatting
 {
     internal class FormattingContext
     {
+        internal ParsedToken CurrentToken { get; private set; }
+        internal ParsedToken NextToken { get; private set; }
 
+        internal FormattingContext(ParsedToken currentToken, ParsedToken nextToken)
+        {
+            this.CurrentToken = currentToken;
+            this.NextToken = nextToken;
+        }
+
+        internal bool IsTriviaInBetween(Trivia.TriviaType triviaType)
+        {
+            foreach (Trivia trivia in NextToken.Token.LeadingTrivia)
+            {
+                if (trivia.type == triviaType)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        internal bool CommentsInBetween()
+        {
+            return this.IsTriviaInBetween(Trivia.TriviaType.Comment);
+        }
+
+        internal bool TokensOnSameLine()
+        {
+            return !this.IsTriviaInBetween(Trivia.TriviaType.Newline);
+        }
         
 
     }
