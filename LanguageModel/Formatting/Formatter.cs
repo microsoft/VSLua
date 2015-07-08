caa2879fb455024d79a4f1d42bb5e0d3269dc5fd
Luca Duran <luca.duran@hotmail.com>
2015-07-08 09:44:19 -0700

    Add Formatter and TextEditInfo objects

---
+++ LanguageModel/Formatting/Formatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageModel.Formatting
+{
+    public static class Formatter
+    {
+        public static List<TextEditInfo> Format(string span)
+        {
+            return new List<TextEditInfo>();
+        }
+    }
+}
