517c614e82d7168d00aa1ca2835de695fab2593a
Luca Duran <luca.duran@hotmail.com>
2015-07-08 17:38:42 -0700

    Update Rules

--- LanguageModel/Formatting/Ruling/Rules.cs
+++ LanguageModel/Formatting/Ruling/Rules.cs
@@ -13,19 +13,19 @@ namespace LanguageModel.Formatting.Ruling
 
         internal static Rule SpaceAfterComma =
             new Rule(
-                new RuleDescriptor(TokenType.Punctuation, TokenRange.Any),
+                new RuleDescriptor(TokenType.Comma, TokenRange.Any),
                 new ContextFilter[] { TokensAreOnSameLine },
                 RuleAction.Space);
 
         internal static Rule SpaceAfterAssignmentOperator =
             new Rule(
-                new RuleDescriptor(TokenRange.Any, TokenType.Operator),
+                new RuleDescriptor(TokenRange.Any, TokenType.AssignmentOperator),
                 new ContextFilter[] { TokensAreOnSameLine },
                 RuleAction.Space);
 
         internal static Rule SpaceBeforeAssignmentOperator =
             new Rule(
-                new RuleDescriptor(TokenRange.Any, TokenType.Operator),
+                new RuleDescriptor(TokenRange.Any, TokenType.AssignmentOperator),
                 new ContextFilter[] { TokensAreOnSameLine },
                 RuleAction.Space);
 
