2cb2ddabde0643696f324bcaae3f2d3c4d1effe2
Luca Duran <luca.duran@hotmail.com>
2015-07-13 11:44:39 -0700

    Add basic common rules

--- LanguageModel/Formatting/Ruling/Rules.cs
+++ LanguageModel/Formatting/Ruling/Rules.cs
@@ -9,25 +9,67 @@ namespace LanguageModel.Formatting.Ruling
     internal class Rules
     {
 
-        // A few test rules for testing
+        private static readonly List<ContextFilter> defaultFilters = new List<ContextFilter>
+        {
+            TokensAreOnSameLine,
+            NoCommentBetweenTokens
+        };
 
         internal static Rule SpaceAfterComma =
             new Rule(
                 new RuleDescriptor(TokenType.Comma, TokenRange.Any),
-                new List<ContextFilter> { TokensAreOnSameLine },
-                RuleAction.Space);
+                defaultFilters, RuleAction.Space);
 
         internal static Rule SpaceAfterAssignmentOperator =
             new Rule(
                 new RuleDescriptor(TokenRange.Any, TokenType.AssignmentOperator),
-                new List<ContextFilter> { TokensAreOnSameLine },
-                RuleAction.Space);
+                defaultFilters, RuleAction.Space);
 
         internal static Rule SpaceBeforeAssignmentOperator =
             new Rule(
                 new RuleDescriptor(TokenRange.Any, TokenType.AssignmentOperator),
-                new List<ContextFilter> { TokensAreOnSameLine },
-                RuleAction.Space);
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceAfterBinaryOperator =
+            new Rule(
+                new RuleDescriptor(TokenRange.BinaryOperators, TokenRange.Any),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceBeforeBinaryOperator =
+            new Rule(
+                new RuleDescriptor(TokenRange.Any, TokenRange.BinaryOperators),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceAfterValueBeforeOpenParenthesis =
+            new Rule(
+                new RuleDescriptor(TokenRange.Value, TokenType.OpenParen),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceBeforeValueAfterOpenParenthesis =
+            new Rule(
+                new RuleDescriptor(TokenType.OpenParen, TokenRange.Value),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceBeforeValueAfterOpenSquareBracket =
+            new Rule(
+                new RuleDescriptor(TokenType.OpenBracket, TokenRange.Value),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceBeforeValueAfterOpenCurlyBrace =
+            new Rule(new RuleDescriptor(TokenType.OpenCurlyBrace, TokenRange.Value),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceAfterValueBeforeCloseParenthesis =
+            new Rule(new RuleDescriptor(TokenRange.Value, TokenType.CloseParen),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceAfterValueBeforeCloseSquareBracket =
+            new Rule(new RuleDescriptor(TokenRange.Value, TokenType.CloseBracket),
+                defaultFilters, RuleAction.Space);
+
+        internal static Rule SpaceAfterValueBeforeCloseCurlyBrace =
+            new Rule(new RuleDescriptor(TokenRange.Value, TokenType.CloseCurlyBrace),
+                defaultFilters, RuleAction.Space);
 
 
 
@@ -36,7 +78,7 @@ namespace LanguageModel.Formatting.Ruling
             return formattingContext.TokensOnSameLine();
         }
 
-        internal static bool NoCommentInBetweenTokens(FormattingContext formattingContext)
+        internal static bool NoCommentBetweenTokens(FormattingContext formattingContext)
         {
             return !formattingContext.CommentsInBetween();
         }
--- LanguageModel/Formatting/TokenRange.cs
+++ LanguageModel/Formatting/TokenRange.cs
@@ -9,6 +9,51 @@ namespace LanguageModel.Formatting
     internal static class TokenRange
     {
         internal static List<TokenType> Any = TokenRange.Fill(Enum.GetValues(typeof(TokenType)));
+        internal static List<TokenType> BinaryOperators =
+            new List<TokenType>
+            {
+                TokenType.AndBinop,
+                TokenType.BitwiseAndOperator,
+                TokenType.BitwiseLeftOperator,
+                TokenType.BitwiseOrOperator,
+                TokenType.BitwiseRightOperator,
+                TokenType.EqualityOperator,
+                TokenType.ExponentOperator,
+                TokenType.FloorDivideOperator,
+                TokenType.GreaterOrEqualOperator,
+                TokenType.GreaterThanOperator,
+                TokenType.LessOrEqualOperator,
+                TokenType.LessThanOperator,
+                TokenType.MinusOperator,
+                TokenType.ModulusOperator,
+                TokenType.MultiplyOperator,
+                TokenType.NotEqualsOperator,
+                TokenType.OrBinop,
+                TokenType.PlusOperator,
+                TokenType.StringConcatOperator,
+                TokenType.TildeUnOp,
+                TokenType.VarArgOperator,
+            };
+
+        internal static List<TokenType> Brackets = new List<TokenType>
+        {
+            TokenType.OpenBracket,
+            TokenType.CloseBracket,
+            TokenType.OpenCurlyBrace,
+            TokenType.CloseCurlyBrace,
+            TokenType.OpenParen,
+            TokenType.CloseParen,
+        };
+
+        internal static List<TokenType> Value = new List<TokenType>
+        {
+            TokenType.Number,
+            TokenType.String,
+            TokenType.FalseKeyValue,
+            TokenType.TrueKeyValue,
+            TokenType.NilKeyValue,
+            TokenType.Identifier,
+        };
 
         private static List<TokenType> Fill(Array values)
         {
