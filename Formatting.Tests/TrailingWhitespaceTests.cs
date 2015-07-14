1920f7616f92ee43e01830842ed71fb0c3b3feb0
Luca Duran <luca.duran@hotmail.com>
2015-07-14 09:03:19 -0700

    Add tests for trailing whitespace

--- Formatting.Tests/TrailingWhitespaceTests.cs
+++ Formatting.Tests/TrailingWhitespaceTests.cs
@@ -7,6 +7,49 @@ namespace Formatting.Tests
         delegate void TestFunction(string original, string expected);
         TestFunction GeneralTest = Tester.GeneralTest;
 
+        [Fact]
+        public void Basic()
+        {
+            string original = "x = 10      ";
+            string expected = "x = 10";
+            GeneralTest(original, expected);
+        }
+
+        [Fact]
+        public void Comments1()
+        {
+            string original = @"x = 10            -- hello world       ";
+            GeneralTest(original, original);
+        }
+
+        [Fact]
+        public void Comments2()
+        {
+            string original = "x = 10    --[[comment]]     ";
+            string expected = "x = 10    --[[comment]]";
+            GeneralTest(original, expected);
+        }
+
+        [Fact]
+        public void MultipleLines()
+        {
+            string original = @"
+x = 10           
+-- comment here
+--[[ block
+comment 
+here]]
+x = x + 1            ";
+            string expected = @"
+x = 10
+-- comment here
+--[[ block
+comment
+here]]
+x = x + 1";
+            GeneralTest(original, expected);
+        }
+
 
     }
 }
