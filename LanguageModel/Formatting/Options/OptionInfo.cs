c8b0080a364b1393695106642f409022094fc99d
Luca Duran <luca.duran@hotmail.com>
2015-07-24 12:46:48 -0700

    Add OptionalRule enum

---
+++ LanguageModel/Formatting/Options/OptionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageService.Formatting.Options
+{
+    public class OptionInfo
+    {
+        public OptionInfo(uint tabSize)
+        {
+            this.TabSize = tabSize;
+        }
+
+        internal uint TabSize { get; }
+    }
+}
---
+++ LanguageModel/Formatting/Options/OptionalRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageService.Formatting.Options
+{
+    public enum OptionalRule
+    {
+        WrappingOneLineForFors,
+        WrappingOneLineForFunctions,
+        WrappingOneLineForTableConstructors,
+        WrappingMoreLinesForTableConstructors,
+        SpaceBeforeOpenParenthesis,
+        SpaceOnInsideOfParenthesis,
+        SpaceOnInsideOfCurlyBrackets,
+        SpaceOnInsideOfSquareBrackets,
+        SpaceAfterCommas,
+        SpaceBeforeAndAfterBinaryOperations,
+        SpaceBeforeAndAfterAssignmentForField,
+        SpaceBeforeAndAfterAssignmentForStatment,
+        FormattingInFor
+    }
+}
