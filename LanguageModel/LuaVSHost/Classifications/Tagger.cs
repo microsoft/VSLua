02ff5e2dd1ac083ecdd8ddcde48b5ffaf4cf7405
Luca Duran <luca.duran@hotmail.com>
2015-08-05 17:38:24 -0700

    Add ITaggerProvider with Tagger

---
+++ LanguageModel/LuaVSHost/Classifications/Tagger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageService;
+using Microsoft.VisualStudio.Language.StandardClassification;
+using Microsoft.VisualStudio.LanguageServices.Lua.Shared;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace Microsoft.VisualStudio.LanguageServices.Lua.Classifications
+{
+    internal class Tagger : ITagger<ClassificationTag>
+    {
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        private ITextBuffer textBuffer;
+        private IStandardClassificationService standardClassifications;
+        private ISingletons singletons;
+
+        internal Tagger(ITextBuffer textBuffer, IStandardClassificationService standardClassifications, ISingletons singletons)
+        {
+            Requires.NotNull(textBuffer, nameof(textBuffer));
+            Requires.NotNull(standardClassifications, nameof(standardClassifications));
+            Requires.NotNull(singletons, nameof(singletons));
+
+            this.textBuffer = textBuffer;
+            this.standardClassifications = standardClassifications;
+            this.singletons = singletons;
+        }
+
+        public IEnumerable<ITagSpan<ClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            foreach (SnapshotSpan span in spans)
+            {
+                ITextSnapshot textSnapshot = span.Snapshot;
+                SourceText sourceText = this.singletons.SourceTextCache.Get(textSnapshot);
+
+                // I don't think ColorEdits is a great name, I just use it because I can't think of anything better right now.
+                //
+                // List<ColorEdit> colorEdits = this.core.FeatureContainer.Colorizer.Colorize(sourceText)
+                //
+                //foreach (ColorEdit colorEdit in colorEdits)
+                //{
+                //    ClassificationTag vsClassification = this.classifications[colorEdit.Classification];
+                //    SnapshotSpan tokenSpan = new SnapshotSpan(textSnapshot, colorEdit.Start, colorEdit.Length);
+
+                //    yield return TagSpan<ClassificationTag>(tokenSpan, vsClassification);
+                //}
+
+                yield break;
+            }
+        }
+    }
+}
