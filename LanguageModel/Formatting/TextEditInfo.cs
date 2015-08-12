1cf4c3f1abea29e57f7e44885bee7c4f3c84ed74
Luca Duran <luca.duran@hotmail.com>
2015-08-12 09:31:14 -0700

    Add value documentation to TextEditInfo

--- LanguageModel/Formatting/TextEditInfo.cs
+++ LanguageModel/Formatting/TextEditInfo.cs
@@ -16,19 +16,22 @@ namespace LanguageService.Formatting
         }
 
         /// <summary>
-        /// The Start index of the text that is to be replaced.
+        /// Gets the Start index of the text that is to be replaced.
         /// </summary>
+        /// <value>The start of the text for the replacing string</value>
         public int Start { get; }
 
         /// <summary>
-        /// The length of the text from the start that is to be replaced.
+        /// Gets the length of the text from the start that is to be replaced.
         /// Length can be 0.
         /// </summary>
+        /// <value>The length of the text that is to be replaced</value>
         public int Length { get; }
 
         /// <summary>
-        /// The string that will replace the text in question.
+        /// Gets the string that will replace the text in question.
         /// </summary>
+        /// <value>The string that spaces the text in question</value>
         public string ReplacingWith { get; }
     }
 }
