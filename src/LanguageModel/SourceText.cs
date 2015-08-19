cd2e981ecaf4ad168d74032d2b5059b2b9463077
Luca Duran <luca.duran@hotmail.com>
2015-08-19 10:26:31 -0700

    Add SourceTextLine and Lines to SourceText

--- src/LanguageModel/SourceText.cs
+++ src/LanguageModel/SourceText.cs
@@ -4,7 +4,11 @@
 *                                                        *
 *********************************************************/
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
+using System.Text;
+using LanguageService.Text;
 using Validation;
 
 namespace LanguageService
@@ -15,10 +19,76 @@ namespace LanguageService
         {
             Requires.NotNull(text, nameof(text));
             this.text = text;
+            this.Lines = this.GetLines();
         }
 
         private string text;
 
+        public ImmutableArray<SourceTextLine> Lines { get; }
+
         public TextReader TextReader => new StringReader(this.text);
+
+        public int? GetLineNumberFromIndex(int index)
+        {
+            for (int i = 0; i < this.Lines.Length; ++i)
+            {
+                if (index >= this.Lines[i].Start && index < this.Lines[i].End)
+                {
+                    return i;
+                }
+            }
+
+            return null;
+        }
+
+        private ImmutableArray<SourceTextLine> GetLines()
+        {
+            List<SourceTextLine> lines = new List<SourceTextLine>();
+
+            StringBuilder currentLine = new StringBuilder();
+
+            int start = 0;
+
+            bool addLine = false;
+
+            for (int i = 0; i < this.text.Length; ++i)
+            {
+                if (addLine)
+                {
+                    addLine = false;
+                    lines.Add(new SourceTextLine(currentLine.ToString(), start, i - 1 - start));
+                    currentLine.Clear();
+                    start = i - 1;
+                }
+
+                currentLine.Append(this.text[i]);
+
+                if (this.text[i] == '\r')
+                {
+                    addLine = true;
+
+                    if (i < this.text.Length - 1 || this.text[i + 1] == '\n')
+                    {
+                        i++;
+                        currentLine.Append(this.text[i]);
+                        continue;
+                    }
+
+                    continue;
+                }
+
+                if (this.text[i] == '\n')
+                {
+                    addLine = true;
+                }
+            }
+
+            if (currentLine.Length > 0)
+            {
+                lines.Add(new SourceTextLine(currentLine.ToString(), start, this.text.Length - start));
+            }
+
+            return lines.ToImmutableArray();
+        }
     }
 }
---
+++ src/LanguageModel/Text/SourceTextLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageService.Text
+{
+    public class SourceTextLine
+    {
+        internal SourceTextLine(string text, int start, int length)
+        {
+            this.Text = text;
+            this.Start = start;
+            this.Length = length;
+            this.End = start + length;
+        }
+
+        public string Text { get; }
+
+        public int Start { get; }
+
+        public int Length { get; }
+
+        public int End { get; }
+
+        public TextReader TextReader => new StringReader(this.Text);
+    }
+}
