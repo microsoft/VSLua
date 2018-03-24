84cdb02477c95c10d34228b7083a358e4bcc4f04
Andrew Arnott <andrewarnott@gmail.com>
2018-03-24 15:11:40 -0700

    Use Nerdbank.GitVersioning

--- src/ProjectLauncher/Properties/AssemblyInfo.cs
+++ src/ProjectLauncher/Properties/AssemblyInfo.cs
@@ -20,6 +20,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
